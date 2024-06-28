48c10122a7b28e4387dc81ff6bc6c5f85b25924a
Erik Pfeffer <epfeffer@terpmail.umd.edu>
2024-06-28 13:22:52 -0400

    Updated .gitignore, index.html, index.js, and removed style.css; added new CSS classes and styles, modified JavaScript code for hamburger menu and overlay, and updated HTML structure and content.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-document.getElementById("hamburger").addEventListener("click", () => {
-  document.getElementById("mobile-nav-links").classList.add("show");
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerMenu = document.getElementById("close");
+  const overlay = document.getElementById("overlay");
+  const mobileNavLinks = document.getElementById("mobile-nav-links");
+  const breakpoint = 1024;
 
-document.getElementById("close").addEventListener("click", () => {
-  document.getElementById("mobile-nav-links").classList.remove("show");
+  // Function to remove the 'show' classes based on screen size
+  const checkScreenSize = () => {
+    if (window.innerWidth >= breakpoint) {
+      hamburgerMenu.classList.remove("active");
+      mobileNavLinks.classList.remove("show");
+      overlay.classList.remove("show");
+    }
+  };
+
+  // Initial check on page load
+  checkScreenSize();
+
+  hamburgerMenu.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active");
+    mobileNavLinks.classList.toggle("show");
+    overlay.classList.toggle("show");
+  });
+
+  // Close the menu and overlay when clicking the overlay
+  overlay.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    mobileNavLinks.classList.remove("show");
+    overlay.classList.remove("show");
+  });
+
+  // Listen for window resize events
+  window.addEventListener("resize", checkScreenSize);
 });
